0c67009b661c9ab561ba37a50c98a3d24bfaadbf
yahyacdr <yahya.zahouani7@gmail.com>
2024-02-01 11:45:47 +0100

    Added main.js without style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { tempMovieData, tempWatchedData } from "./movies_data";
 import Header from "./components/header";
+import Main from "./components/main";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Main watchList={tempMovieData} watchedList={tempWatchedData}></Main>
     </div>
   );
 }
---
+++ src/components/main.js
@@ -0,0 +1,92 @@
+export default function Main(props) {
+  function countAverage(arr, prop) {
+    const sum = arr.reduce((acc, curr) => acc + curr[prop], 0);
+    return sum / arr.length;
+  }
+  return (
+    <main>
+      <div className="watchList">
+        {props.watchList.map((wl) => (
+          <WatchList
+            poster={wl.Poster}
+            title={wl.Title}
+            releaseYear={wl.Year}
+            key={wl.imdbID}
+          ></WatchList>
+        ))}
+      </div>
+      <div className="watchedList">
+        <WatchedListCounter
+          count={props.watchedList.length}
+          averageRate={countAverage(props.watchedList, "imdbRating")}
+          averageMyRate={countAverage(props.watchedList, "userRating")}
+          averageDuration={countAverage(props.watchedList, "runtime")}
+        ></WatchedListCounter>
+        <div className="list">
+          {props.watchedList.map((wdl) => (
+            <WatchedList
+              poster={wdl.Poster}
+              title={wdl.Title}
+              rate={wdl.imdbRating}
+              myRate={wdl.userRating}
+              duration={wdl.runtime}
+              key={wdl.imdbID}
+            ></WatchedList>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+function WatchList(props) {
+  return (
+    <>
+      <div className="panel">
+        <div className="img-cntnr">
+          <img src={props.poster} alt="movie-poster" />
+        </div>
+        <div className="ttl-date">
+          <h2 className="title">{props.title}</h2>
+          <p className="date">📅 {props.releaseYear}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function WatchedList(props) {
+  return (
+    <>
+      <div className="panel">
+        <div className="img-cntnr">
+          <img src={props.poster} alt="movie-poster" />
+        </div>
+        <div className="ttl-details">
+          <h2 className="title">{props.title}</h2>
+          <div className="details">
+            <p className="date">⭐ {props.rate}</p>
+            <p className="date">🌟 {props.myRate}</p>
+            <p className="date">⏳ {props.duration} min</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function WatchedListCounter(props) {
+  return (
+    <>
+      <div className="counter-cntnr">
+        <h2>MOVIES YOU WATCHED</h2>
+        <div className="details">
+          <p>#️⃣ {props.count} movies</p>
+          <p>⭐️ {props.averageRate}</p>
+          <p>🌟 {props.averageMyRate}</p>
+          <p>⏳ {props.averageDuration} min</p>
+        </div>
+      </div>
+    </>
+  );
+}

7f6136b64ec1f1506ec5d38e54913c7dab482fe5
yahyacdr <yahya.zahouani7@gmail.com>
2024-02-01 09:44:40 +0100

    Remove redundant tags from App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import { tempMovieData, tempWatchedData } from "./movies_data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;

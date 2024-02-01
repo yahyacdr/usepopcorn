8b56c4a88555461136ef902a3fdc66b27439d8f2
yahyacdr <yahya.zahouani7@gmail.com>
2024-02-01 10:53:33 +0100

    App.js was modified a bit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { tempMovieData, tempWatchedData } from "./movies_data";
+import Header from "./components/header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;

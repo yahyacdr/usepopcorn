0575403e146bd0f23bc0d76934cdbc7cd4b9660f
Yahya Z <yahya.z@gmail.com>
2024-02-13 15:50:06 +0100

    Jonas style (Finished creating the star rating)

--- src/components/starRating.js
+++ src/components/starRating.js
@@ -1,59 +1,87 @@
 import { useState } from "react";
-import "./starRating.css";
+import PropTypes from "prop-types";
 
-export default function StarRating({ count = 5 }) {
-  const [switchStar, setSwitchStar] = useState(0);
-  const [starRated, setStarRated] = useState(3);
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+};
+
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc719",
+  size = 32,
+}) {
+  const cntnrStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: "16px",
+  };
+
+  const starCntntStyle = {
+    display: "flex",
+    gap: "4px",
+  };
+
+  const txtStyle = {
+    margin: "0",
+    color: color,
+    fontSize: `${size / 2}px`,
+  };
+  const [rating, setRating] = useState(0);
+  const [hoverStar, setHoverStar] = useState(0);
   return (
-    <>
-      <div className="star-cntnr">
-        {Array.from({ length: count }, (_, i) => (
-          <span key={i}>
-            <Star
-              starI={i + 1}
-              setSwitchStar={setSwitchStar}
-              switchStar={switchStar}
-              rated={starRated}
-              setStarRated={setStarRated}
-            />
-          </span>
+    <div style={cntnrStyle}>
+      <div style={starCntntStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            changeRate={() => setRating(i + 1)}
+            full={rating >= i + 1}
+            hover={hoverStar >= i + 1}
+            onHoverIn={() => setHoverStar(i + 1)}
+            onHoverOut={() => setHoverStar(0)}
+            size={size}
+            color={color}
+          />
         ))}
-        <p>{starRated}</p>
       </div>
-    </>
+      <p style={txtStyle}>{hoverStar || rating || ""}</p>
+    </div>
   );
 }
 
-function Star(props) {
-  const setStarCondition = props.switchStar
-    ? props.switchStar + 1 <= props.starI
-    : props.rated < props.starI;
-  console.log(props.switchStar);
+function Star({ changeRate, full, hover, onHoverIn, onHoverOut, size, color }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
   return (
-    <>
-      {!setStarCondition ? (
+    <span
+      role="button"
+      style={starStyle}
+      onClick={changeRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
+      {full || hover ? (
         <svg
-          onMouseEnter={() => props.setSwitchStar(props.starI)}
-          onMouseLeave={() => props.setSwitchStar(0)}
-          onClick={() => props.setStarRated(props.starI)}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill={"#FF5733"}
-          stroke={"#FF5733"}
-          index={props.starI}
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
       ) : (
         <svg
-          onMouseEnter={() => props.setSwitchStar(props.starI)}
-          onMouseLeave={() => props.setSwitchStar(0)}
-          onClick={() => props.setStarRated(props.starI)}
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke={"#FF5733"}
-          index={props.starI}
+          stroke={color}
         >
           <path
             strokeLinecap="round"
@@ -63,6 +91,39 @@ function Star(props) {
           />
         </svg>
       )}
-    </>
+    </span>
   );
 }
+
+/*
+FULL STAR
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 20 20"
+  fill="#000"
+  stroke="#000"
+>
+  <path
+    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+  />
+</svg>
+
+
+EMPTY STAR
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="#000"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="{2}"
+    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+  />
+</svg>
+
+*/
